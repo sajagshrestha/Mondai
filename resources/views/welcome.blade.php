d02dba1d16084bed351308d0c9ec99e597b47662
Purushottam Shrestha <purushrestha4@hotmail.com>
2021-05-08 17:10:54 +0545

    fixed index.js

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,7 +22,7 @@
     </head>
     <body>
         <div id="app"></div>
-        <script src="{{ asset('js/app.js') }}"></script>
+        <script src="{{ asset('js/index.js') }}"></script>
       </body>
 
 </html>
