4de0dfc328ba2df84b18cdfaab85e2a8e37878df
Sajag Shrestha <sajagshrestha0852@gmail.com>
2021-05-09 09:38:30 +0545

    react router setup

--- resources/js/components/App.tsx
+++ resources/js/components/App.tsx
@@ -1,12 +1,33 @@
-import React, { useState } from "react";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./Home/Home";
+import Login from "./Login/Login";
+import Page404 from "./Page404";
 
 const App = () => {
-
-
     return (
         <div>
-            <h1>Users list</h1>
-            BOBO
+            <Router>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/login">Login</Link>
+                    </li>
+                </ul>
+                <Switch>
+                    <Route path="/login" exact>
+                        <Login />
+                    </Route>
+                    <Route path="/" exact>
+                        <Home />
+                    </Route>
+                    <Route>
+                        <Page404 />
+                    </Route>
+                </Switch>
+            </Router>
         </div>
     );
 };
