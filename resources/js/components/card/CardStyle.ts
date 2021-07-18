a174bde55420ee88070c3d84e5f89971b79b8a79
puru9860 <purushrestha4@hotmail.com>
2021-07-18 09:46:26 +0545

    updated list and card

--- resources/js/components/card/CardStyle.ts
+++ resources/js/components/card/CardStyle.ts
@@ -1,26 +1,35 @@
 import styled from "styled-components";
+import Avatar from "@material-ui/core/Avatar";
 
 export const CardContainer = styled.div`
-  background: #292929;
-  display: grid;
-  color: #f2f2f2;
-  grid-template-rows: 1fr 1fr 1fr;
-  grid-row-gap: 5px;
-  padding: 1rem;
-  width: 100%;
-  height: 100px;
-  margin-bottom: 5px;
+    background: #292929;
+    display: grid;
+    color: #f2f2f2;
+    grid-template-rows: 1fr 1fr 1fr;
+    grid-row-gap: 5px;
+    padding: 1rem;
+    width: 100%;
+    height: 100px;
+    margin-bottom: 5px;
 `;
 
 export const CardLabel = styled.div`
-  height: 2px;
-  background: #47fb79;
-  border-radius: 10px;
+    height: 2px;
+    background: #47fb79;
+    border-radius: 10px;
 `;
 
 export const CardTitle = styled.div``;
 export const CardFooter = styled.div`
-  display: flex;
-  justify-content: space-between;
+    display: flex;
+    justify-content: flex-end;
 `;
+
+export const StyledAvatar = styled(Avatar)`
+    &&& {
+        height: 25px;
+        width: 25px;
+    }
+`;
+
 export const DueDate = styled.div``;
--- resources/js/components/list/List.tsx
+++ resources/js/components/list/List.tsx
@@ -2,119 +2,48 @@ import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import axios from "axios";
 import { BASE_URL, getAuthHeader } from "../../services/index";
-import Button from "@material-ui/core/Button";
-import TextField from "../common/TextField";
-import "./style.css";
+import Card from "../card/Card";
+import AddCard from "./AddCard";
+
 
 const ListContainer = styled.div`
     background: #151514;
     color: #ffffff;
     padding: 1rem;
     width: 350px;
+    margin-right: 1rem;
+
 `;
-const AddCardContainer = styled.div`
-    background: #151514;
-    color: #ffffff;
-    margin-top: 10px;
-    width:100%;
-`;
-import Card from "../card/Card";
-const List = () => {
+
+const List: React.FC<{list: any}> = ({list}) => {
     const [cards, setCards] = useState<any>([]);
-    const [formDisplay, setFormDisplay] = useState(false);
-    const [card, setCard] = useState<any>({
-        title: "",
-        user_id: 1,
-    });
+
+    const appendCard = (card: any): void => {
+        setCards([...cards, card]);
+    };
 
     useEffect(() => {
         axios
-            .get(`${BASE_URL}/card/1`, {
+            .get(`${BASE_URL}/card/${list.id}`, {
                 headers: getAuthHeader(),
             })
-            .then((response) => {
+            .then((response: any) => {
                 setCards(response.data.data);
             })
-            .catch((error) => {
+            .catch((error: any) => {
                 console.log(error);
             });
     }, []);
 
-    const onChangeHandler = (event: any) => {
-        setCard({
-            ...card,
-            title: event.target.value
-        });
-    };
-    const onSubmitHandler = (event: any) => {
-        event.preventDefault();
-        axios
-            .post("/api/card/1", card, {
-                headers: getAuthHeader(),
-            })
-            .then(response => {
-                console.log(response.data.data[0]);
-                setCards([...cards, response.data.data[0]]);
-            })
-            .catch(err => {
-                console.log(err);
-            });
-        setCard({
-            ...card,
-            title: ""
-        });
-        toggleHandler()
-    };
-
-    const toggleHandler = () => {
-        setFormDisplay(!formDisplay);
-    };
-
-
     return (
         <ListContainer>
-            <h3>Todo</h3>
+            <h3>{list.name}</h3>
             {cards.map((c: any) => (
                 <div key={c.id}>
                     <Card card={c} />
                 </div>
             ))}
-            <AddCardContainer>
-                {formDisplay ? (
-                    <div className="mt-3">
-                        <form onSubmit={onSubmitHandler}>
-
-                            <TextField
-                                multiline
-                                rows={4}
-                                placeholder="Enter card title"
-                                variant="outlined"
-                                className="add"
-                                fullWidth
-                                value={card.title}
-                                onChange={onChangeHandler}
-                            />
-                            <div>
-                                <Button variant="outlined" color="default" type="submit">
-                                    Add card
-                                </Button>
-                                <Button color="default" onClick={toggleHandler}>
-                                    X
-                                </Button>
-                            </div>
-                        </form>
-
-                    </div>
-                ) : (
-                    <Button
-                        color="default"
-                        className="w-100 add"
-                        onClick={toggleHandler}
-                    >
-                        + Add another card
-                    </Button>
-                )}
-            </AddCardContainer>
+            <AddCard appendCard={appendCard} list_id={list.id}/>
         </ListContainer>
     );
 };
